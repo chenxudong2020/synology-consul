92708c21c295b340e451a53bc3c3b6b6c3269c96
chenxudong2020 <872603935@qq.com>
2023-11-11 14:31:13 +0800

    Update tempature.sh

--- run_scripts/tempature.sh
+++ run_scripts/tempature.sh
@@ -6,16 +6,16 @@ do
     echo "----------------$cpu_temp" >> ./unas.log
    if (( $(echo "$cpu_temp >= 75" | /opt/bin/bc -l) )); then
         echo "大于等于75" >> ./unas.log
-	    /usr/sbin/i2cset -y 0 0x54 0xf0 255
+	    sudo /usr/sbin/i2cset -y 0 0x54 0xf0 255
     elif (( $(echo "$cpu_temp >= 65" | /opt/bin/bc -l) )) && (( $(echo "$cpu_temp < 75" | /opt/bin/bc -l) )); then
         echo "大于65小于等于75" >> ./unas.log
-	   /usr/sbin/i2cset -y 0 0x54 0xf0 180
+	   sudo /usr/sbin/i2cset -y 0 0x54 0xf0 180
     elif (( $(echo "$cpu_temp >= 55" | /opt/bin/bc -l) )) && (( $(echo "$cpu_temp < 65" | /opt/bin/bc -l) )); then
         echo "大于等于55小于65" >> ./unas.log
-		/usr/sbin/i2cset -y 0 0x54 0xf0 150
+		sudo /usr/sbin/i2cset -y 0 0x54 0xf0 150
     else
 		 echo "55度以下" >> ./unas.log
-		 /usr/sbin/i2cset -y 0 0x54 0xf0 100
+		 sudo /usr/sbin/i2cset -y 0 0x54 0xf0 100
 	fi
 	sleep 60
 done
