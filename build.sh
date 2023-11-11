806aa1a8c4cdf162e75683873a1bcb5befb0c537
chenxudong2020 <872603935@qq.com>
2023-11-11 14:26:24 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,8 +10,10 @@ mkdir -p ./package/bin/
 chmod +x ./run_scripts/*
 \cp -rf ./run_scripts/* ./package/bin/
 
+
+
 ./info.sh "${PACKAGE_VERSION}" > INFO
 tar -cvzf package.tgz package
-tar -cvf package.spk INFO LICENSE conf/ package.tgz scripts/pre* scripts/post* scripts/start*
+tar -cvf package.spk PACKAGE_ICON_256.PNG PACKAGE_ICON.PNG INFO LICENSE conf/ package.tgz scripts/pre* scripts/post* scripts/start*
 rm package.tgz
 mv package.spk "unas_${unas_VERSION}_${OS}_${ARCH}.spk"
