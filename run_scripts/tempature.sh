822b7ea1bb8e46fb0e1496dc4ed6d1c0fba36710
chenxudong2020 <872603935@qq.com>
2023-11-11 14:13:19 +0800

    Update tempature.sh

--- run_scripts/tempature.sh
+++ run_scripts/tempature.sh
@@ -3,17 +3,18 @@
 while true
 do
     cpu_temp=$(/opt/sbin/sensors | grep -i "Core 0" | awk '{print $3}' | grep -oP "\d+\.\d+")
+    echo "----------------$cpu_temp" >> ./unas.log
    if (( $(echo "$cpu_temp >= 75" | /opt/bin/bc -l) )); then
-        echo "大于等于75"
+        echo "大于等于75" >> ./unas.log
 	    i2cset -y 0 0x54 0xf0 255
     elif (( $(echo "$cpu_temp >= 65" | /opt/bin/bc -l) )) && (( $(echo "$cpu_temp < 75" | /opt/bin/bc -l) )); then
-        echo "大于65小于等于75"
+        echo "大于65小于等于75" >> ./unas.log
 	   i2cset -y 0 0x54 0xf0 180
     elif (( $(echo "$cpu_temp >= 55" | /opt/bin/bc -l) )) && (( $(echo "$cpu_temp < 65" | /opt/bin/bc -l) )); then
-        echo "大于等于55小于65"
+        echo "大于等于55小于65" >> ./unas.log
 		i2cset -y 0 0x54 0xf0 150
     else
-		 echo "55度以下"
+		 echo "55度以下" >> ./unas.log
 		 i2cset -y 0 0x54 0xf0 100
 	fi
 	sleep 60
