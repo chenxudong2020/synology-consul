1679a80b51a9fd27a8d06c7e6d3d7aac41de15d8
chenxudong2020 <872603935@qq.com>
2023-11-13 20:30:30 +0800

    dddd

--- package/start.sh
+++ package/start.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
 
-APPNAME="tempature"
-
-owpid="$(ps -ef | grep ${APPNAME} | grep -v 'grep' | awk '{print $2}')"
-    if [ -z "${owpid}" ]; then
-        /var/packages/${SYNOPKG_PKGNAME}/target/package/bin/run.sh
-    fi
-exit 0    
+/var/packages/unas/target/package/bin/run.sh
