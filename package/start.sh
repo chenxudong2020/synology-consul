c8d8bdc080e73f6c7062b93540821e2fe1adc362
chenxudong2020 <872603935@qq.com>
2023-11-13 17:05:53 +0800

    ff

--- package/start.sh
+++ package/start.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
-source /var/packages/unas/var/env.sh
-/var/packages/unas/target/package/bin/run.sh
+APPNAME="tempature"
+
+owpid="$(ps -ef | grep ${APPNAME} | grep -v 'grep' | awk '{print $2}')"
+    if [ -z "${owpid}" ]; then
+        /var/packages/unas/target/package/bin/run.sh
+    fi
+exit 0    
--- package/stop.sh
+++ package/stop.sh
@@ -2,3 +2,4 @@
 
 
 /var/packages/unas/target/package/bin/tempature_off.sh
+exit 0
